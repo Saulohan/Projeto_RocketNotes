8225292435c59d8a6ccf562c0b2a691a59e13c7b
unknown <saulolohan09@gmail.com>
2023-03-25 19:18:47 -0300

    commit

--- RocketNotes/src/Pages/Details/index.jsx
+++ RocketNotes/src/Pages/Details/index.jsx
@@ -1,14 +1,27 @@
-import {Container} from "./styles.js"
-import { Button } from "../../components/button/index.jsx"
+import {Container, Links} from "./styles.js";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/button";
+import { Section } from "../../components/Section";
+
+
 
 export function Details(){
 
   return(
 
     <Container>
-      <h1>Hello World!</h1>
-      <h2>dasdsa</h2>
-      <Button />
+      <Header/>
+
+      <Section title="Links úteis">
+        <Links>
+          <li> <a href="#">https://www.rocketseat.com.br/</a>  </li>
+          <li> <a href="#">https://www.rocketseat.com.br/</a>  </li>         
+        </Links>
+      </Section>
+
+      <Section></Section>
+  
+      <Button title="Voltar" />
 
     </Container>
   )
---
+++ RocketNotes/src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile,Logout } from "./style";
+
+export function Header(){
+
+    return(
+
+        <Container>
+            <Profile>
+                <img 
+                    src="https://github.com/saulohan.png"
+                    alt="Foto do usuário"
+                />
+
+                <div>
+                    <span>Bem-Vindo</span>
+                    <strong>Saulo Lohan</strong>
+                </div>
+                
+            </Profile>
+            
+            <Logout>
+                <RiShutDownLine />
+            </Logout>
+
+        </Container>
+    )
+}
