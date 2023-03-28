a322e13a8e378bdb5c039bd3f182128b09cd852d
unknown <saulolohan09@gmail.com>
2023-03-28 17:30:54 -0300

    commit

---
+++ RocketNotes/src/Pages/Home/index.jsx
@@ -0,0 +1,56 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+
+import { Header } from '../../components/Header'
+import { ButtonText } from '../../components/buttonText';
+import { FiPlus, FiSearch } from 'react-icons/fi'
+import { Input } from '../../components/Input';
+import {Section} from '../../components/Section'
+import {Note} from '../../components/Note'
+
+
+export function Home() {
+
+    return(
+
+        <Container>
+
+            <Brand>
+                <h1> RocketNotes </h1>
+            </Brand>
+
+            <Header />
+
+            <Menu>              
+                <li> <ButtonText title={"Todos"} isActive/></li>
+                <li> <ButtonText title={"React"}/></li>
+                <li> <ButtonText title={"Nodejs"}/></li>
+            </Menu>
+
+            <Search>
+                <Input icon= {FiSearch} placeholder="Pesquisar pelo titulo"/>
+            </Search>
+
+            <Content>
+                    <Section title="Minhas Notas"> 
+
+                        <Note data={ {
+                            title : 'Reack',
+                            tags : [
+                                {id: '1', name : 'react'}, 
+                                {id: '2', name : 'rocketseat'}
+                            ],
+                            } } 
+                        />
+
+                    </Section>
+
+            </Content>
+
+            <NewNote>
+                <FiPlus/>
+                Criar Nota
+            </NewNote>
+
+        </Container>
+    );
+}
--- RocketNotes/src/main.jsx
+++ RocketNotes/src/main.jsx
@@ -4,12 +4,17 @@ import {Details} from './Pages/Details';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './Styles/global';
 import theme from './Styles/theme';
+import { Home } from './Pages/Home'
+import { SignIn } from './Pages/SignIn'
+import { SignUp } from './Pages/SignUp'
+import { Profile } from './Pages/Profile'
+import { New } from './Pages/New'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme} > 
       <GlobalStyles />
-      <Details />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
 )
